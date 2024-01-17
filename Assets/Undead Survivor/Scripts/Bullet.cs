a3ec5a360a7ad12170b2c2ef3ebc0b9a709a2bf1
superajiwon <superajiwon@naver.com>
2024-01-17 18:12:13 +0900

    Enemy Animation & HUD
    1. Enemy Animation
     - Hit Animation & Dead Animation
     - Using Animation Function
    2. Set UI
     - Make HUD for user
     - Visualizing Exp, Health, Kill, Timer, and Level

--- Assets/Undead Survivor/Scripts/Bullet.cs
+++ Assets/Undead Survivor/Scripts/Bullet.cs
@@ -7,6 +7,9 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
+    float tempDamage;
+    float lowerDamage;
+
     Rigidbody2D rigid;
 
     void Awake()
@@ -31,6 +34,9 @@ public class Bullet : MonoBehaviour
         this.damage = damage;
         this.per = per;
 
+        tempDamage = this.damage;
+        lowerDamage = damage / 3;
+
         if (per > -1)
         {
             rigid.velocity = dir * 15f;
@@ -43,9 +49,11 @@ public class Bullet : MonoBehaviour
             return;
 
         per--;
+        damage -= lowerDamage;
 
-        if (per == -1)
+        if (per == -1 || damage <= 0)
         {
+            damage = tempDamage;
             rigid.velocity = Vector2.zero;
             gameObject.SetActive(false);
         }
--- Assets/Undead Survivor/Scripts/Enemy.cs
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -14,19 +14,24 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D col;
     Animator anim;
     SpriteRenderer spriter;
+
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        col= GetComponent<Collider2D>();
         spriter = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        if (!isLive)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
             return;
 
         // 방향
@@ -51,6 +56,10 @@ public class Enemy : MonoBehaviour
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isLive = true;
+        col.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         health = maxHealth;
     }
     
@@ -64,20 +73,40 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isLive)
             return;
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if (health > 0)
         {
             // Live -> hit action
+            anim.SetTrigger("Hit");
         }
         else
         {
             // Dead
-            Dead();
+            isLive = false;
+            col.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
+    }
+
+    IEnumerator KnockBack()
+    {
+        //yield return null; // 1프레임 쉬기
+        //yield return new WaitForSeconds(2f); // 2초 쉬기
+        yield return wait; // 다음 하나의 물리 프레임 딜레이
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse); // 순간적인 힘 : Impulse
     }
 
     void Dead()
